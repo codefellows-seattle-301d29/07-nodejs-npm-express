68fc41f40cf6aa237974101304c494ae801927c6
Taylor Stemple <taylorstemple@gmail.com>
2018-01-18 10:59:17 -0800

    final commit

--- ryan_taylor/server.js
+++ ryan_taylor/server.js
@@ -6,23 +6,26 @@ const app = express();
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-app.post('/articles', bodyParser, function(request, response) {
-  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
-  console.log(request.body);
-  response.send('Record posted to server!!');
-})
-
-app.listen(PORT, () => {
-  console.log('currently listening on port:', PORT);
-});
-
 app.use(express.static('./public'));
 
+//Our files are now in a public directory because they are the files that are going to be utilized by the client. 
+
 app.get('/new', (req, res) => {
-  res.sendFile('new.html', {'root': './public'});
-  console.log('testing this function')
+  res.sendFile('new.html', {root: './public'});
+  console.log('testing this function');
 });
+
+app.post('/articles', bodyParser, function(req, res) {
+  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
+  console.log(req.body);
+  res.send('Record posted to server!!');
+})
 
 app.use((req, res) => {
   res.status(404).send('Page not Found!')
 });
+      
+
+app.listen(PORT, () => {
+        console.log('currently listening on port:', PORT);
+      });
