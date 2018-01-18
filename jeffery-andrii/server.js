920c7686e712ffe3a4434752b9095491310bc64a
andriiglukhyi <andrigluxii@gmail.com>
2018-01-18 11:20:53 -0800

    work fine

--- jeffery-andrii/server.js
+++ jeffery-andrii/server.js
@@ -2,7 +2,7 @@
 
 // REVIEW: There is a package here called body-parser, which is used by the provided POST route. Be sure to install that and save it as a dependency after you create your package.json.
 
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3026;
 const express = require('express');
 const app = express();
 
@@ -10,21 +10,24 @@ const app = express();
 
 // need this to serve any static file in  public directory
 
-app.use(express.static('/public'));
+app.use(express.static('./public'));
 
 app.listen(PORT, () =>{
   console.log('curently on port ', PORT);
   });
 
 app.get('/new', (req, res)=>{
-  res.sendfile('public/new.html');
+  res.sendfile('new.html', {root: './public'});
 });
 
-app.get('/index', (req, res)=>{
-  res.sendfile('public/index.html');
-});
+const bodyParser = require('body-parser').urlencoded({extended: true});
 
-app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-});
+app.post('/articles', bodyParser, function(request, response) {
+  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
+  console.log(request.body);
+  response.send('Record posted to server!!');
+})
+
+app.use(function (req, res, next) {
+  res.status(404).send("WRONG WAY DUDE")
+})
