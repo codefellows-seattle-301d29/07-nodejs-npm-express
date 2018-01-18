bf8dfbf996b1cf93e66c13eb2906cf5f6ea43378
Brian Bixby <brianbixby0@gmail.com>
2018-01-18 10:30:27 -0800

    404 error works

--- bessie-melanie-brian/server.js
+++ bessie-melanie-brian/server.js
@@ -8,18 +8,26 @@ const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
 // Our client facing files are now in public so we can tell express to serve those static files in that directory.
+// all static file that we serve comes from the public directory, but when we serve these files we need to specify the root again
 app.use(express.static('./public'));
 
-app.get('/new', (req, res) => {
-  res.sendFile('/public/new.html', {'root':'./public/'});
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {'root':'./public/'});
 });
-// Why do we have to specify the root if it's already taken care of on line 11?
 
-app.post('/articles', bodyParser, function(req, res) {
+app.get('/new', (req, res) => {
+  res.sendFile('new.html', {'root':'./public/'});
+});
+
+app.get('/*', (req, res) => {
+  res.status(404).send('you just made a request');
+});
+
+app.post('/articles', bodyParser, (req, res) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(req.body);
   res.send('Record posted to server!!');
-})
+});
 
 app.listen(PORT, () => {
   console.log('listening on port:', PORT);
